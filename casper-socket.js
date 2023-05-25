ba7095ac1a4718051cdcccac5433402e1ed0fce6
Américo Gomes <americo.gomes@cloudware.pt>
2023-05-25 17:41:36 +0100

    Adjusts for epaper 'focus' message handling.

--- casper-socket.js
+++ casper-socket.js
@@ -927,7 +927,7 @@ export class CasperSocket extends HTMLElement {
       invokeId = parseInt(data);
       if (!isNaN(invokeId)) {
         const offset = invokeId.toString().length;
-        if (data.substring(offset, offset + 3) === ':D:' || data.substring(offset, offset + 3) === ':n:' || data.substring(offset, offset + 3) === ':J:' ) {
+        if (data.substring(offset, offset + 3) === ':D:' || data.substring(offset, offset + 3) === ':n:' || data.substring(offset, offset + 3) === ':J:' || data.substring(offset, offset + 3) === ':F:' ) {
           const documentHandler = this._documents.get(invokeId);
           if (documentHandler) {
             documentHandler(data.substring(offset + 1), invokeId);
